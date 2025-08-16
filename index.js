7c91a69c36e54c9dfe3dfc877e49efa0cec050df
sagarbarahate <sagarbarahate19@gmail.com>
2025-08-16 12:55:05 +0530

    l

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var session = require("express-session");
 var upload = require("express-fileupload");
 var adminroute = require("./routes/admin");
 var userroute = require("./routes/user");
-require("dotenv");
+require("dotenv").config();
 var app = express();
 
 app.use(bodyparser.urlencoded({extended:true}));
