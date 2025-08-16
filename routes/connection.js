af9a83c97824cd684d4f4ded05fcc5ad3a32199d
sagarbarahate <sagarbarahate19@gmail.com>
2025-08-16 12:58:15 +0530

    c

--- routes/connection.js
+++ routes/connection.js
@@ -2,10 +2,10 @@ var mysql = require("mysql");
 var util = require("util");
 
 var conn = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"",
-    database:"insurance_project"
+    host:"bvdhyquzfvdps41odb8h-mysql.services.clever-cloud.com",
+    user:"ufuycywoe21qzyvh",
+    password:"AojoIgeALEgv9e0Irg6e",
+    database:"bvdhyquzfvdps41odb8h"
 });
 
 var exe = util.promisify(conn.query).bind(conn);
